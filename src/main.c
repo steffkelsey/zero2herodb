a8878a9fbe93c319700661900199b89a38a3b25a
steffkelsey <thecodeviking@gmail.com>
2026-01-14 14:23:57 -0500

    Hardens up adding an employee

--- src/main.c
+++ src/main.c
@@ -11,7 +11,8 @@ void print_usage(char *argv[]) {
   printf("Usage: %s -n -f <database file>\n", argv[0]);
   printf("\t -n - create new database file\n");
   printf("\t -f - [required] path to database file\n");
-  printf("\t -a - append employee information\n");
+  printf("\t -l - list the employees\n");
+  printf("\t -a - add via CSV list of (name,address,hours)\n");
   return;
 }
 
@@ -19,13 +20,14 @@ int main(int argc, char *argv[]) {
   char *addstring = NULL;
   char *filepath = NULL;
   bool newfile = false;
+  bool list = false;
   int c;
 
   int dbfd = -1;
   struct dbheader_t *dbhdr = NULL;
   struct employee_t *employees = NULL;
 
-  while ((c = getopt(argc, argv, "nf:a:")) != -1) {
+  while ((c = getopt(argc, argv, "nlf:a:")) != -1) {
     switch (c) {
       case 'a':
         addstring = optarg;
@@ -35,6 +37,9 @@ int main(int argc, char *argv[]) {
         break;
       case 'n':
         newfile = true;
+        break;
+      case 'l':
+        list = true;
         break;
       case '?':
         printf("Unknown option -%c\n", c);
@@ -80,10 +85,11 @@ int main(int argc, char *argv[]) {
   }
 
   if (addstring) {
-    dbhdr->count++;
-    employees = realloc(employees, dbhdr->count*(sizeof(struct employee_t)));
+    add_employee(dbhdr, &employees, addstring);
+  }
 
-    add_employee(dbhdr, employees, addstring);
+  if (list) {
+    list_employees(dbhdr, employees);
   }
 
   output_file(dbfd, dbhdr, employees);
--- src/parse.c
+++ src/parse.c
@@ -10,18 +10,38 @@
 #include "common.h"
 #include "parse.h"
 
-//void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
-//
-//}
+void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
+  printf("list employees called!\n");
+}
 
-int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees, char *addstring) {
+int add_employee(struct dbheader_t *dbhdr, struct employee_t **employees, char *addstring) {
+  if (NULL == dbhdr) return STATUS_ERROR;
+  if (NULL == employees) return STATUS_ERROR;
+  if (NULL == *employees) return STATUS_ERROR;
+  if (NULL == addstring) return STATUS_ERROR;
+
   char *name = strtok(addstring, ",");
-  char *addr = strtok(NULL, ",");
-  char *hours = strtok(NULL, ",");
+  if (NULL == name) return STATUS_ERROR;
 
-  strncpy(employees[dbhdr->count-1].name, name, sizeof(employees[dbhdr->count-1].name));
-  strncpy(employees[dbhdr->count-1].address, addr, sizeof(employees[dbhdr->count-1].address));
-  employees[dbhdr->count-1].hours = atoi(hours);
+  char *addr = strtok(NULL, ",");
+  if (NULL == addr) return STATUS_ERROR;
+
+  char *hours = strtok(NULL, ",");
+  if (NULL == hours) return STATUS_ERROR;
+
+  struct employee_t *e = *employees;
+  e = realloc(e, sizeof(struct employee_t)*dbhdr->count+1);
+  if (e == NULL) {
+    return STATUS_ERROR;
+  }
+
+  dbhdr->count++;
+
+  strncpy(e[dbhdr->count-1].name, name, sizeof(e[dbhdr->count-1].name));
+  strncpy(e[dbhdr->count-1].address, addr, sizeof(e[dbhdr->count-1].address));
+  e[dbhdr->count-1].hours = atoi(hours);
+
+  *employees = e;
 
   return STATUS_SUCCESS;
 }
