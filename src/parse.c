1eed3719df0462e3a0d7265d0229084cbb0ef088
steffkelsey <thecodeviking@gmail.com>
2026-01-14 16:02:11 -0500

    Removes unused file descriptor from create db header

--- src/parse.c
+++ src/parse.c
@@ -151,7 +151,7 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
   *headerOut = header;
 }
 
-int create_db_header(int fd, struct dbheader_t **headerOut) {
+int create_db_header(struct dbheader_t **headerOut) {
   struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
   if (header == NULL) {
     printf("Malloc failed to create db header\n");
