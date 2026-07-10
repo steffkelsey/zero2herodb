246acedfaea94b5b2bda076a65ea3ecbdd376ec6
steffkelsey <thecodeviking@gmail.com>
2026-07-10 15:04:58 -0400

    removes const from arguments

--- src/kv.c
+++ src/kv.c
@@ -7,7 +7,7 @@
 //   - val: a pointer to the value to hash
 //   - capacity: the capacity of the db
 // returns: 
-size_t hash(const char *val, int capacity) {
+size_t hash(char *val, int capacity) {
   size_t hash = 0x13371337deadbeef;
 
   while(*val) {
@@ -29,7 +29,7 @@ size_t hash(const char *val, int capacity) {
 //   - value: a pointer to the value
 // returns: the index of the key, otherwise on 
 // error, returns -1. on not found, return -2
-int kv_put(kv_t *db, const char *key, const char *value) {
+int kv_put(kv_t *db, char *key, char *value) {
   if (!db || !key || !value) return -1;
 
   size_t idx = hash(key, db->capacity);
